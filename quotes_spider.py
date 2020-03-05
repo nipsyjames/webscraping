84901936217616802bc86f44473013492608dd2e
nipsyjames <49446071+nipsyjames@users.noreply.github.com>
2020-03-05 20:57:13 +0530

    scrapy with containers

--- quotes_spider.py
+++ quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import QuotetutorialItem
 
 class QuotesSpider(scrapy.spiders.Spider):
      name='quotes'
@@ -7,5 +8,17 @@ class QuotesSpider(scrapy.spiders.Spider):
 
      ]
      def parse(self,response):
-         title=response.css('title').extract()
-         yield {'titletext':title}
+         items = QuotetutorialItem()
+         #title=response.css('title').extract()
+         #yield {'titletext':title}
+        all_div_quotes = response.css('div.quote')
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
